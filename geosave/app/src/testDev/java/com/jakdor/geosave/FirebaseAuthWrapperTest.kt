5a4acda6a665e7bc07c9bad57a28333f162b7f38
Jakub Dorda <jak_dor@wp.pl>
2018-09-13 19:14:43 +0200

    add Copyright notice

--- geosave/app/src/testDev/java/com/jakdor/geosave/FirebaseAuthWrapperTest.kt
+++ geosave/app/src/testDev/java/com/jakdor/geosave/FirebaseAuthWrapperTest.kt
@@ -1,3 +1,11 @@
+/*
+ * GeoSave - app for easy sharing and collaborating on GPS related data
+ * Copyright (C) 2018  Jakub Dorda
+ *
+ * Software under GPLv3 licence - full copyright notice available at:
+ * https://github.com/jakdor/geosave/blob/master/README.md
+ */
+
 package com.jakdor.geosave
 
 import com.google.firebase.auth.FirebaseAuth
